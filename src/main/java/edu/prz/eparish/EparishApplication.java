cc817eb3cee06a8946f7d76422cd1167a958d3d5
Maciej Koryl <maciej.koryl@asseco.pl>
2026-04-28 14:57:28 +0200

    Wersja startowa

---
+++ src/main/java/edu/prz/eparish/EparishApplication.java
@@ -0,0 +1,13 @@
+package edu.prz.eparish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EparishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EparishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/prz/eparish/EparishApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.prz.eparish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EparishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
